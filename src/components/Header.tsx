980487343ae950c06c5c3daaf3426b7e01f6690d
Takhmina <thmnshmv@gmail.com>
2021-06-04 14:06:51 +0600

    remove unused components

---
+++ src/components/Header.tsx
@@ -0,0 +1,91 @@
+
+
+const links = [
+  {
+    title: 'О КГЮА',
+    routes: [
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      },
+      {
+        title: 'Информация о КГЮА ',
+        route: ''
+      }
+    ]
+  },
+  {
+    title: 'Учебные подразделения',
+    routes: [
+
+    ]
+  },
+  {
+    title: 'Наука',
+    routes: [
+
+    ]
+  },
+  {
+    title: 'Международные сотрудничества',
+    routes: [
+
+    ]
+  },
+  {
+    title: 'Поступающим',
+    routes: [
+
+    ]
+  },
+  {
+    title: 'Сотрудникам',
+    routes: [
+
+    ]
+  },
+  {
+    title: 'Студентам',
+    routes: [
+
+    ]
+  },
+
+]
+
+export const Header = () => {
+  return (
+    <div>
+      <img />
+      <ul>
+        {links?.map((item) => {
+          <li></li>
+        })}
+      </ul>
+    </div>
+  )
+}
--- src/components/TextBox.tsx
+++
@@ -1,7 +0,0 @@
-export type TextBoxProps = {};
-
-const TextBox: React.FC<TextBoxProps> = ({ children }: any) => {
-  return <div className="text-box">{children}</div>;
-};
-
-export default TextBox
