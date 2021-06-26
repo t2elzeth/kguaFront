a0e494bc5edfa065bd64709a04c2b355a1e7ef42
Takhmina <thmnshmv@gmail.com>
2021-06-26 15:53:18 +0600

    fix documents page

--- src/pages/about/local-acts.tsx
+++ src/pages/about/local-acts.tsx
@@ -1,11 +1,44 @@
 import { AboutPage } from '../../templetes'
+import { Download } from '@Components'
 
 export type LocalActsPageProps = {}
+const documents = [
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  },
+  {
+    title: "Отзыв ведущей организации на диссертацию Джумалиева Д.С.",
+    src: ""
+  }
+]
 
 const LocalActsPage: React.FC<LocalActsPageProps> = () => {
   return (
     <AboutPage pageName="Документы" pageTitle='Документы' image='/images/first-section.png'>
-
+      {documents.map((item) => (
+        <Download document={item} />
+      ))}
     </AboutPage>
   )
 }
