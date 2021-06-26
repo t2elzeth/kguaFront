62d0ce0c45c946aed8cab6a7f588152c3aa981e1
Takhmina <thmnshmv@gmail.com>
2021-06-26 15:08:08 +0600

    fix symbolism page

--- k8s/deploy-checker.sh
+++
@@ -1,8 +0,0 @@
-if ! kubectl rollout status -w deployment "ioka-landing"; then
-  kubectl rollout restart deployment "ioka-landing"
-  if ! kubectl rollout status -w deployment "ioka-landing"; then
-    kubectl rollout undo deployment "ioka-landing"
-    exit 1
-  fi
-  kubectl rollout status -w deployment "ioka-landing"
-fi
