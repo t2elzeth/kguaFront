86082da1e37a2f4385e665a752637927231e3548
sadabaevb <beka.sadabaev@gmail.com>
2021-08-25 18:21:19 +0600

    bachelor

--- src/pages/incoming/bachelor-degree.tsx
+++ src/pages/incoming/bachelor-degree.tsx
@@ -1,54 +1,27 @@
+import { Download } from '@src/components'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
 import { AboutPage } from '../../templates'
 
 const BachelorDegree: React.FC = () => {
   const { t } = useTranslation('incoming')
+
+  const documents = [
+    {
+      title: 'Правила приема 2021-2022 КГЮА ВПО рус',
+      document: '/documents/bachelor/Правила приема 2021-2022 КГЮА ВПО рус.pdf',
+    },
+  ]
   return (
     <AboutPage
       t={t}
-      pageName="Бакалавриат"
-      pageTitle="Бакалавриат"
+      pageName={t('bachelor.title')}
+      pageTitle={t('bachelor.title')}
       image="/images/incoming-title-2.png"
     >
-      <p>
-        Подготовка специалистов в институте Адвокатуры и юстиции осуществляется по двум
-        направлениям:
-        <br />
-        1) Направление «Юриспруденция»
-        <br />
-        Профили:
-        <br />
-        <ul>
-          «Адвокатура»;
-          <br />
-          «Юстиция»;
-          <br />
-          «Информационное право».
-          <br />
-        </ul>
-        Профиль «Адвокатура» готовит специалистов в области адвокатской деятельности.
-        <br />
-        Профиль «Юстиция» выпускает кадры, специализирующиеся в области правосудия,
-        правоохранительной и правоприменительной деятельности.
-        <br />
-        Профиль «Информационное право» направлен на подготовку юристов в сфере информационного
-        права.
-        <br />
-        2) Направление «Правоохранительная деятельность» по профилю «Административная деятельность».
-        <br />
-        <br />
-        Срок обучения: 4 года
-        <br />
-        <br />
-        Магистерская программа по направлению «Юриспруденция: <br />
-        Профили: <br />
-        <ul>
-          «Информационное право и информационная безопасность»; <br />
-          «Адвокатура и нотариат». <br />
-        </ul>
-        Срок обучения: 2 года
-      </p>
+      {documents.map((item) => (
+        <Download key={item.title} document={item} />
+      ))}
     </AboutPage>
   )
 }
