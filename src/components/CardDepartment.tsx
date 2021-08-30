2b439211b6835b685163233374bed5c3e4a5ddfc
sadabaevb <beka.sadabaev@gmail.com>
2021-08-30 17:34:59 +0600

    css

--- src/components/CardDepartment.tsx
+++ src/components/CardDepartment.tsx
@@ -9,23 +9,30 @@ import React from 'react'
 
 import { Grid } from '@material-ui/core'
 import Link from 'next/link'
+import { useTranslation } from 'react-i18next'
 
 export type CardDepartmentProps = {
   items: any
 }
 
 export const CardDepartment: React.FC<CardDepartmentProps> = ({ items }) => {
+  const { t } = useTranslation('common')
   return (
-    <Grid container justifyContent="space-between" direction="row">
+    <Grid container justifyContent="space-between" direction="row" className="blocksGrid">
       {items?.map((item: any) => (
         <Link key={item.title} href={`/about/structure/departments/department/${item.id}`}>
           <Grid item xs={12} md={5} className="block">
             <a style={{ cursor: 'pointer' }}>
-              {/* <h4 className="title">{item.title}</h4>
-              <p>Заведующий кафедрой: </p>
-              <span>{item.name}</span>
+              <h4 className="title">{item.title}</h4>
+              <p>
+                Заведующий кафедрой:{' '}
+                <span style={{ fontWeight: 500, fontSize: 15 }}>
+                  {t(item?.head_teacher?.position)}
+                </span>
+              </p>
+              <span style={{ fontSize: 17 }}>{item?.head_teacher?.full_name}</span>
               <p>Контактные данные: </p>
-              <span>{item.contacts}</span> */}
+              <span>{item.contacts.phone}</span>
             </a>
           </Grid>
         </Link>
