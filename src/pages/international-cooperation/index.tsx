d947928588b2bc5661c34df558045b56f2025fc1
sadabaevb <beka.sadabaev@gmail.com>
2021-09-01 09:37:35 +0600

    international-coopeartion ishovered change color image

--- src/pages/international-cooperation/index.tsx
+++ src/pages/international-cooperation/index.tsx
@@ -1,11 +1,14 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { WithoutSideBar } from '../../templates'
 
 const InternationalCooperation = () => {
   const { t } = useTranslation('international-cooperation')
   const categories = t('sidebar_list', { returnObjects: true })
+
+  const [isHovered, setIsHovered] = useState({})
 
   return (
     <WithoutSideBar pageName={t('title')}>
@@ -20,10 +23,13 @@ const InternationalCooperation = () => {
         <h2>{t('title')}</h2>
         <p>В настоящее время в составе КГЮА действуют следующие институты:</p>
         <div className="categories">
-          {Array.from(categories).map((item: any, index) => (
+          {Array.from(categories).map((item: any, index: number) => (
             <Link key={index} href={item.route}>
-              <div>
-                <img src={item.img} alt="" />
+              <div
+                onMouseEnter={() => setIsHovered({ [index]: true })}
+                onMouseLeave={() => setIsHovered({ [index]: false })}
+              >
+                <img src={isHovered[index] ? item.img_hov : item.img} alt="" />
                 <p>{item.name}</p>
               </div>
             </Link>
